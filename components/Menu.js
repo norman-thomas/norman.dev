493dcfcedbb8214ccfb75790d042cad66c4f8b6c
Norman Thomas <news@hiroki.de>
2021-04-22 09:06:22 +0200

    disable link in menu

--- components/Menu.js
+++ components/Menu.js
@@ -3,16 +3,16 @@ import Link from "next/link"
 const Menu = () => {
   return (
     <div className="w-full flex justify-end bg-primary-400 py-4 text-white">
-      <nav className="text-center font-extralight text-lg w-1/2 xl:w-1/3 flex">
+      <nav className="text-center font-extralight text-lg w-1/2 xl:w-1/4 flex">
         <Link href="/">
           <a className="hover:font-bold flex-1">Home</a>
         </Link>
         <Link href="/blog/">
           <a className="hover:font-bold flex-1">Blog</a>
         </Link>
-        <Link href="https://norman.dev/">
+        {/*<Link href="https://norman.dev/">
           <a className="hover:font-bold flex-1">Resume</a>
-        </Link>
+        </Link>*/}
       </nav>
     </div>
   )
